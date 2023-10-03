a6c49fbae2105c8d96d6aee3cff5999d5431ccfd
Developersunil001 <sunilkumar088839@gmail.com>
2023-10-04 00:44:59 +0530

    Introduction in js

---
+++ intro.js
@@ -0,0 +1,27 @@
+// Declare your name as a string and print its length in js 
+
+let Name = 'sunil';
+console.log(Name.length);
+
+// Declare your first name as a string and print its 1st character
+
+let firstName = 'Sunil';
+console.log(firstName[0]);
+
+// Declare your first name as a string and print its last character
+
+let phelaName = 'Sunil';
+console.log([phelaName.length - 1]);
+
+//what are the lenth of an empty strings & a String with a single space
+let empty = "";
+console.log(empty.length);
+let space = " ";
+console.log(space.length);
+
+// output of "apnacollege" + 123
+let a = "apnacollege" + 123;
+console.log(a)
+
+
+
