8a6614f5456824a05614bc1cf34393dd0f080af9
Developersunil001 <sunilkumar088839@gmail.com>
2023-10-05 01:06:21 +0530

    impove

--- conditions.js
+++ conditions.js
@@ -1,50 +1,50 @@
 // Traffic light management system
-let color = 'red';
+// let color = 'red';
 
-if (color == 'red') {
-    console.log("stop");
-}
-if (color == 'yellow') {
-    console.log("wait");
-}
-if (color == 'green') {
-    console.log("go");
-}
+// if (color == 'red') {
+//     console.log("stop");
+// }
+// if (color == 'yellow') {
+//     console.log("wait");
+// }
+// if (color == 'green') {
+//     console.log("go");
+// }
 
 // Create a System to calculate popcorn price based on the size customer asked for
 
-let popcornSize = 'o'
+// let popcornSize = 'o'
 
-if (popcornSize === 'XL') {
-    console.log('price is Rs.250');
-}
-else if (popcornSize === 'L') {
-    console.log('price is Rs.200');
-}
-else if (popcornSize === 'M') {
-    console.log('price is Rs.100');
-}
-else if (popcornSize === 'S') {
-    console.log('price is Rs.50');
-}
-else {
-    console.log('Yeah size hamare pass nahi hai');
-}
+// if (popcornSize === 'XL') {
+//     console.log('price is Rs.250');
+// }
+// else if (popcornSize === 'L') {
+//     console.log('price is Rs.200');
+// }
+// else if (popcornSize === 'M') {
+//     console.log('price is Rs.100');
+// }
+// else if (popcornSize === 'S') {
+//     console.log('price is Rs.50');
+// }
+// else {
+//     console.log('Yeah size hamare pass nahi hai');
+// }
 
 // Nested Ladder
 
-let marks = 40;
-if (marks >= 33) {
-    console.log('You are pass');
-    if (marks >= 80) {
-        console.log("Grade : A")
-    }
-    else {
-        console.log("Grade : B")
-    }
-} else {
-    console.log("next time better luck")
-}
+// let marks = 40;
+// if (marks >= 33) {
+//     console.log('You are pass');
+//     if (marks >= 80) {
+//         console.log("Grade : A")
+//     }
+//     else {
+//         console.log("Grade : B")
+//     }
+// } else {
+//     console.log("next time better luck")
+// }
 
 // Logical Operator
 // logical operator to combine expression
@@ -55,45 +55,57 @@ if (marks >= 33) {
 // False --> False |  True
 // False --> False |  False
 
-let marks = 90;
-if (marks >= 33 && marks <= 80) {
-    console.log("You are pass");
-    console.log("Grade : A");
-}
-else {
-    console.log("You are Fail");
-}
+// let marks = 90;
+// if (marks >= 33 && marks <= 80) {
+//     console.log("You are pass");
+//     console.log("Grade : A");
+// }
+// else {
+//     console.log("You are Fail");
+// }
 
-// Logical OR Operator ==> OR operator me koi ek bhi condition true ho gyi toh anser true ayega 
+// Logical OR Operator ==> OR operator me koi ek bhi condition true ho gyi toh anser true ayega
 //  True  | True -->  True
 //  True  | False --> True
 //  False | True -->  True
 //  False | False --> False
 
 // let marks = 90;
-if (marks >= 33 || marks <= 80) {
-    console.log("You are pass");
-    console.log("Grade : A");
-}
-else {
-    console.log("You are Fail");
-}
+// if (marks >= 33 || marks <= 80) {
+//     console.log("You are pass");
+//     console.log("Grade : A");
+// }
+// else {
+//     console.log("You are Fail");
+// }
 
-// Logical NOT Operator 
+// Logical NOT Operator
 
-// Jab hum combined logic ko create krte hai i.e. ki jisme logical AND,OR,NOT present 
+// Jab hum combined logic ko create krte hai i.e. ki jisme logical AND,OR,NOT present
 // ho usme conditions ko check left to right kiya jata hai .
 
-let stuMarks = 50;
-if(!(stuMarks > 100)){
-    console.log("Pass ho gya bhaai");
-}
+// let stuMarks = 50;
+// if(!(stuMarks > 100)){
+//     console.log("Pass ho gya bhaai");
+// }
 
-let Marks = 90;
-if ((Marks >= 33 || Marks <= 80) && !Marks) {
-    console.log("You are pass");
-    console.log("Grade : A");
+// let Marks = 90;
+// if ((Marks >= 33 || Marks <= 80) && !Marks) {
+//     console.log("You are pass");
+//     console.log("Grade : A");
+// }
+// else {
+//     console.log("You are Fail");
+// }
+
+// Practice Q.
+// A "good string" is a string starts with the letter "a" & has a length > 3.
+// write a programs to find if a string good or bad 
+
+let str = 'apple';
+if (str[0] === 'a' && str.length > 3) {
+    console.log('string is good');
 }
 else {
-    console.log("You are Fail");
+    console.log('string is bad');
 }
