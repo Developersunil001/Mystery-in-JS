7e4fbea6af22f8f71493d424e6f31cecd84ddd51
Developersunil001 <sunilkumar088839@gmail.com>
2023-10-05 00:35:25 +0530

    Conditionals in Js

---
+++ conditions.js
@@ -0,0 +1,99 @@
+// Traffic light management system
+let color = 'red';
+
+if (color == 'red') {
+    console.log("stop");
+}
+if (color == 'yellow') {
+    console.log("wait");
+}
+if (color == 'green') {
+    console.log("go");
+}
+
+// Create a System to calculate popcorn price based on the size customer asked for
+
+let popcornSize = 'o'
+
+if (popcornSize === 'XL') {
+    console.log('price is Rs.250');
+}
+else if (popcornSize === 'L') {
+    console.log('price is Rs.200');
+}
+else if (popcornSize === 'M') {
+    console.log('price is Rs.100');
+}
+else if (popcornSize === 'S') {
+    console.log('price is Rs.50');
+}
+else {
+    console.log('Yeah size hamare pass nahi hai');
+}
+
+// Nested Ladder
+
+let marks = 40;
+if (marks >= 33) {
+    console.log('You are pass');
+    if (marks >= 80) {
+        console.log("Grade : A")
+    }
+    else {
+        console.log("Grade : B")
+    }
+} else {
+    console.log("next time better luck")
+}
+
+// Logical Operator
+// logical operator to combine expression
+
+//Logical And && (And operator me agr ek bhi condition false ho gyi toh anser false hi ayega)
+// True  --> True  |  True
+// False --> True  |  False
+// False --> False |  True
+// False --> False |  False
+
+let marks = 90;
+if (marks >= 33 && marks <= 80) {
+    console.log("You are pass");
+    console.log("Grade : A");
+}
+else {
+    console.log("You are Fail");
+}
+
+// Logical OR Operator ==> OR operator me koi ek bhi condition true ho gyi toh anser true ayega 
+//  True  | True -->  True
+//  True  | False --> True
+//  False | True -->  True
+//  False | False --> False
+
+// let marks = 90;
+if (marks >= 33 || marks <= 80) {
+    console.log("You are pass");
+    console.log("Grade : A");
+}
+else {
+    console.log("You are Fail");
+}
+
+// Logical NOT Operator 
+
+// Jab hum combined logic ko create krte hai i.e. ki jisme logical AND,OR,NOT present 
+// ho usme conditions ko check left to right kiya jata hai .
+
+let stuMarks = 50;
+if(!(stuMarks > 100)){
+    console.log("Pass ho gya bhaai");
+}
+
+let Marks = 90;
+if ((Marks >= 33 || Marks <= 80) && !Marks) {
+    console.log("You are pass");
+    console.log("Grade : A");
+}
+else {
+    console.log("You are Fail");
+}
--- intro.js
+++ intro.js
@@ -1,4 +1,5 @@
 // Part - 1
+
 // Declare your name as a string and print its length in js 
 
 let Name = 'sunil';
@@ -15,12 +16,14 @@ let phelaName = 'Sunil';
 console.log([phelaName.length - 1]);
 
 //what are the lenth of an empty strings & a String with a single space
+
 let empty = "";
 console.log(empty.length);
 let space = " ";
 console.log(space.length);
 
 // output of "apnacollege" + 123
+
 let a = "apnacollege" + 123;
 console.log(a)
 
