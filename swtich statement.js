b5120e7e866b1b07c61f47e98ce7c659763fb5ae
Developersunil001 <sunilkumar088839@gmail.com>
2023-10-05 12:22:15 +0530

    practice Ques.

---
+++ swtich statement.js
@@ -0,0 +1,52 @@
+let color = "red";
+
+switch (color) {
+    case "red":
+        console.log("stop");
+        break;
+
+    case "yellow":
+        console.log("slow down");
+        break;
+
+    case "green":
+        console.log("go")
+        break;
+
+    default: console.log("ride carefully");
+}
+
+// use switch statement to print the day of the week using a number variable 'day' with values 1 to 7;
+// 1 = Monday , 2 = Tuesday
+
+let day = 5;
+
+switch(day){
+    case 1 : 
+    console.log("Monday");
+    break;
+
+    case 2 : 
+    console.log("Tuesday");
+    break;
+
+    case 3 : 
+    console.log("Wednesday");
+    break;
+
+    case 4 : 
+    console.log("Thursday");
+    break;
+
+    case 5 : 
+    console.log("Friday");
+    break;
+
+    case 6:
+    console.log("Saturday");
+    break;   
+    
+    default : 
+    console.log("weekend")
+
+}
