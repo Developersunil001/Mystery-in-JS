296d1a70cb56534c330d38040fbbe3213a64217e
Developersunil001 <sunilkumar088839@gmail.com>
2023-11-07 01:20:28 +0530

    Array Interview Question 🔥

---
+++ Strings_interview_Question.js
@@ -0,0 +1,41 @@
+// How to find vowels from string in js
+// How to reverse a string in js
+// Convert first letter of a string into uppercase in js
+// Find no. of occurrences of a character in a string in js
+// Write a function to check if a given string is Palindrome or not
+// Given two strings, how can you check if the strings are anagram for each other?
+// How do you check if a string starts with another string?
+// How do you remove whitespaces from a given string?
+// How do you return a character from a string at a specific index(2)?
+// Create a program to check sequence of brackets(expression) in an array is valid or not
+// Check if array has all unique chars
+// Reverse a String using Java
+// Reverse Each word in String
+// Find Each Characater occurrence in String
+// Find First Non Repeated Character in String
+// Replace Character with its occurrence in string
+// Find Longest Substring without repeated characters
+// Sort String characters using Js
+// Remove Special Characters in String
+// Reverse the string of every word
+// Arrange the string in ascending order
+// How do you reverse a given string in place?
+// How do you print duplicate characters from a string?
+// How do you check if two strings are anagrams of each other?
+// How do you find all the permutations of a string?
+// How can a given string be reversed using recursion?
+// How do you check if a string contains only digits?
+// How do you find duplicate characters in a given string?
+// How do you count a number of vowels and consonants in a given string?
+// How do you count the occurrence of a given character in a string?
+// How do you print the first non-repeated character from a string?
+// How do you convert a given String into int like the atoi()?
+// How do you reverse words in a given sentence without using any library method?
+// How do you check if two strings are a rotation of each other?
+// How do you check if a given string is a palindrome?
+// How do you find the length of the longest substring without repeating characters?
+// Given string str, How do you find the longest palindromic substring in str?
+// How to convert a byte array to String?
+// how to remove the duplicate character from String?
+// How to find the maximum occurring character in given String?
+// How do you remove a given character from String?
