c064f942ae9c57f6b70bfab85199a8191c346736
Developersunil001 <sunilkumar088839@gmail.com>
2023-11-14 00:20:03 +0530

    api

--- API.js
+++ API.js
@@ -19,48 +19,82 @@
 // 400 - Bad Request 
 // 500 - Internal Server Error 
 
-let url1 = "https://catfact.ninja/fact";
+// let url1 = "https://catfact.ninja/fact";
 
-fetch(url)
-     .then((res) => {
-          return res.json();
-     })
-     .then((data1) => {
-          console.log("data1 :", data1.fact);
-          return fetch(url1);
-     })
-     .then((res) => {
-          return res.json();
-     })
-     .then((data2)=>{
-          console.log("data2 :",data2.fact);
-     })
-     .catch((err) => {
-          console.log("Error", err);
-     })
+// fetch(url)
+//      .then((res) => {
+//           return res.json();
+//      })
+//      .then((data1) => {
+//           console.log("data1 :", data1.fact);
+//           return fetch(url1);
+//      })
+//      .then((res) => {
+//           return res.json();
+//      })
+//      .then((data2)=>{
+//           console.log("data2 :",data2.fact);
+//      })
+//      .catch((err) => {
+//           console.log("Error", err);
+//      })
 
-     console.log("Happy Diwali");
+//      console.log("Happy Diwali");
 
 
 // Axios
 
 
-let btn = document.querySelector("button").addEventListener("click", async () => {
-     let fact = await getFacts();
-     let p = document.querySelector("#result");
-     p.innerText  = fact;
-})
+// let btn = document.querySelector("button").addEventListener("click", async () => {
+//      let fact = await getFacts();
+//      let p = document.querySelector("#result");
+//      p.innerText  = fact;
+// })
 
-let url = "https://catfact.ninja/fact";
-async function getFacts() {
+// let url = "https://catfact.ninja/fact";
+// async function getFacts() {
+//      try {
+//           let res = await axios.get(url);
+//           return (res.data.fact);
+//      } catch (error) {
+//           console.log("error hai bhai api me",error);
+//      }
+// }
+
+
+let btn = document.querySelector("button");
+let url2 = "https://dog.ceo/api/breeds/image/random";
+
+btn.addEventListener("click", async () => {
+     let link = getImage();
+     //    console.log(link)
+     let img = document.querySelector("#result");
+     img.setAttribute("src", link);
+     console.log(link)
+});
+
+async function getImage() {
      try {
-          let res = await axios.get(url);
-          return (res.data.fact);
+          let res = await axios.get(url2);
+          return res.data.message
+          // return (res);
      } catch (error) {
-          console.log("error hai bhai api me",error);
+          return "error hai bhai api me", error;
      }
 }
 
+let url = "https://icanhazdadjoke.com";
 
+async function getJokes(){
+     try {
+      const config = {headers : {Accept:"application/json"} }
+      let res = await axios.get(url,config);
+      console.log(res.data)
+     } catch (error) {
+          console.log(error);
+     }
+}
+
+// let url = "https://universities.hipolabs.com/search?name=";
 
 
